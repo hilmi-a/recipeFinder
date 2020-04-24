806b5f23a67256121c1320f0fd1c9b5af2471d56
hilmi-a <hilmi.a.aziz@gmail.com>
2020-04-24 15:43:34 +0700

    adding webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const path = require("path");
 module.exports = {
     entry:"./src/js/index.js",
     output: {
-        path: path.resolve(__dirname,'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname,'dist'),
+        filename: 'js/bundle.js'
     },
-    mode: 'development'
-
+    // mode: 'development'
+    devServer: {
+        contentBase: "./dist"
+    }
 }
